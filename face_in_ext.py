e68313ffff3b2c06d5bfcc80dbbbfc0d3d3798de
Centaurus99 <1102569568@qq.com>
2019-05-20 13:23:33 +0800

    bug修复

--- face_in_ext.py
+++ face_in_ext.py
@@ -5,14 +5,17 @@ import pickle
 import multiprocessing as mp
 
 IMAGE_PATH = 'data/'
-
-def ImageIn(face_DATA, i):
+def ImageIn(face_DATA, i, Sum):
     photo = face_recognition.load_image_file(i)
     name = i[len(IMAGE_PATH):len(i)-4]
     face_locations = face_recognition.face_locations(photo)
     if (len(face_locations) != 1):
-        return 'The number of faces is ' + str(len(face_locations)) + ' but not 1'
-    face_encoding = face_recognition.face_encodings(photo, face_locations, num_jitters = 200)[0]
+        return 'The number of faces in "' + i + '" is ' + str(len(face_locations)) + ' but not 1'
+    face_encoding = face_recognition.face_encodings(photo, face_locations, num_jitters = 1)[0]
+    if name in face_DATA:
+        Sum[1] += 1
+    else:
+        Sum[0] += 1
     face_DATA[name] = face_encoding
     return name
 
@@ -26,16 +29,23 @@ if __name__ =='__main__':
     po = mp.Pool()
     mgr = mp.Manager()
     face_DATA = mgr.dict()
+    Sum = mgr.list()
+    Sum.append(0)
+    Sum.append(0)
+
+    print('Start!')
     with open('faces.dat', 'rb') as f:
         data_in = pickle.load(f)
     for key in data_in:
         face_DATA[key] = data_in[key]
-    print('Start!')
+        
     for i in paths:
-        po.apply_async(func = ImageIn, args = (face_DATA, i),callback = Bar)
+        po.apply_async(func = ImageIn, args = (face_DATA, i, Sum),callback = Bar)
     po.close()
     po.join()
 
+    print('Add ' + str(Sum[0]) + ' peoples')
+    print('Replace ' + str(Sum[1]) + ' peoples')
     print('All ' + str(len(face_DATA)) + ' peoples')
     out_Data = face_DATA.copy()
     with open('faces.dat', 'wb') as f:
